e45aab889e2a089f64d9ddfcb3b6d83e707f5df3
Roman Krotov <krotan152@gmail.com>
2024-03-10 15:26:24 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+while(true){
+    Console.Write("Введите число или 'q' для выхода");
+    string input = Console.ReadLine();
+    if (input == "q"){
+        break;
+    }
+    int number;
+    if(int.TryParse(input, out number)){
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10; 
+        }
+        if (sum%2==0){
+            Console.Write("STOP");
+            break;
+        }
+    else{
+        Console.Write("Введите число или 'q'");
+    }
+    }
+}
