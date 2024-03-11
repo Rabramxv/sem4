e04f0e4379dd26f8af762fa2e9215682dec2335f
Roman Krotov <krotan152@gmail.com>
2024-03-11 16:05:16 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    int[] numbers = new int[10]; 
+    Random random = new Random();
+    int evenCount = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(100, 1000);
+        Console.Write(numbers[i] + " "); 
+        if (numbers[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+    }
+    Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
